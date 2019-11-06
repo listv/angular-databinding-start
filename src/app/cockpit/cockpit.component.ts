133152240d74c9825b0a5adf12f9b05cfc2d6511
Oleksandr Kuziakov <Oleksandr_Kuziakov@epam.com>
2019-11-06 11:46:17 +0200

    Gett Access to the Template & DOM with @ViewChild.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -18,21 +25,23 @@ export class CockpitComponent implements OnInit {
   }>();
   // newServerName = '';
   // newServerContent = '';
+  @ViewChild('serverContentInput', { static: false })
+  serverContentInput: ElementRef;
   constructor() {}
 
   ngOnInit() {}
 
-  onAddServer(nameInput: HTMLInputElement, contentInput:HTMLInputElement) {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: contentInput.value
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint(nameInput: HTMLInputElement, contentInput:HTMLInputElement) {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: contentInput.value
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
